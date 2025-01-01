de11b0bb21e66f29e5a6b6d1090083d0068e230f
Yash Balotiya <balotiyash@gmail.com>
2025-01-01 18:53:21 +0530

    Completed Login Page & Updated Google Reviews

--- controller/encrypt.js
+++ controller/encrypt.js
@@ -1,6 +1,6 @@
 /** 
  * File: controller/encrypt.js
- * Author: Neha Balotia
+ * Author: Yash Balotiya
  * Description: This file contains Logical Part to encrypt and decrypt the text.
  * Created on: 01/01/2025
  * Last Modified: 01/01/2025
@@ -48,29 +48,43 @@ function decrypt(text, shift) {
     text = text.split('').reverse().join('');
     
     let decryptedText = '';
-    
+    let i = 0;
+
     // Remove the random characters and decrypt each remaining character
-    for (let i = 0; i < text.length; i++) {
+    while (i < text.length) {
         let char = text.charCodeAt(i);
         
         // Decrypt uppercase letters
         if (char >= 65 && char <= 90) {
             decryptedText += String.fromCharCode(((char - 65 - shift + 26) % 26) + 65);
+            i++; // Move to the next character
         }
         // Decrypt lowercase letters
         else if (char >= 97 && char <= 122) {
             decryptedText += String.fromCharCode(((char - 97 - shift + 26) % 26) + 97);
+            i++; // Move to the next character
         }
-        // Skip non-alphabetic characters (the random ones)
+        // If it's a non-alphabetic character, just add it to the decrypted text
         else {
-            i++; // Skip the random character that was inserted
+            decryptedText += text[i];
+            i++; // Move to the next character
+        }
+        
+        // Skip the random character that was inserted
+        if (i < text.length) {
+            i++; // Skip the random character
         }
     }
     return decryptedText;
 }
 
+// Decrypt function to reverse the process
+// function j10(k11,l12){k11=k11.split('').reverse().join('');let m13='';let n14=0;while(n14<k11.length){let o15=k11.charCodeAt(n14);if (o15>=65&&o15<=90){m13+=String.fromCharCode(((o15-65-l12+26)%26)+65);n14++;}else if(o15>=97&&o15<=122){m13+=String.fromCharCode(((o15-97-l12+26)%26)+97);n14++;}else{m13+=k11[n14];n14++;}if(n14<k11.length){n14++;}}return m13;}
+
 // Example usage
-const text = "Hello World!";
+// const text = "ganesh.sunariwal@somaiya.edu";
+// const text = "1A2B3C@DJGSonyyyyy@";
+const text = "Hello World";
 const shift = 3;
 
 const encrypted = encrypt(text, shift);
