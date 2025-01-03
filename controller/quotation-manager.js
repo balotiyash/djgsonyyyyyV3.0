96492b46d72ddeeca7a80f94527db0e21433e68f
Yash Balotiya <balotiyash@gmail.com>
2025-01-03 12:55:40 +0530

    Completed Quotation Manager Page

--- controller/quotation-manager.js
+++ controller/quotation-manager.js
@@ -3,22 +3,69 @@
  * Author: Yash Balotiya
  * Description: This file contains all the logical stuffs to handle Quotation Management Page
  * Created on: 27/12/2024
- * Last Modified: 27/12/2024
+ * Last Modified: 03/01/2025
 */
 
-function tallyAmount(value1, value2, total) {
-    const VALUE2 = document.getElementById(value2).value;
-    document.getElementById(total).innerHTML = parseFloat(value1 * VALUE2).toFixed(2);
+function tallyHorizontalAmount() {
+    const quantity = document.querySelectorAll('.qntyInput');
+    const rate = document.querySelectorAll('.rateInput');
+    const total = document.querySelectorAll('.hTotal');
+
+    quantity.forEach((element, index) => {
+        element.addEventListener('input', () => {
+            const totalAmount = element.value * rate[index].value;
+            total[index].innerHTML = totalAmount.toFixed(2);
+
+            let soundTotal = 0;
+            for (let i = 0; i < 5; i++) {
+                soundTotal += parseFloat(total[i].innerHTML);
+            }
+            document.getElementById('soundTotal').innerHTML = parseFloat(soundTotal).toFixed(2);
+
+            let subTotal = 0;
+            total.forEach((element) => {
+                subTotal += parseFloat(element.innerHTML);
+            });
+            document.getElementById('subTotal').innerHTML = parseFloat(subTotal).toFixed(2);
+
+            const discount = document.getElementById('discountInput');
+            let discountAmount = 0;
+            let grandTotal = subTotal;
+
+            discount.addEventListener('input', () => {
+                discountAmount = parseFloat(subTotal * discount.value / 100).toFixed(2);
+                document.getElementById('discountAmount').innerHTML = discountAmount;
+                grandTotal = subTotal - discountAmount;
+                document.getElementById('grandTotal').innerHTML = parseFloat(grandTotal).toFixed(2);
+            });
+
+            document.getElementById('grandTotal').innerHTML = parseFloat(grandTotal).toFixed(2);
+        });
+    });
 }
 
-const options = {
-    margin: 1,
-    filename: `INVOICE_${invoiceData.sale_id}.pdf`,
-    image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-};
+document.getElementById("downloadBtn").addEventListener('click', downloadInvoice);
 
-html2pdf().set(options).from(invoiceElement).save().then(() => {
-    invoiceElement.style.display = 'none';
-});
+function downloadInvoice() {
+    const options = {
+        margin: 0.5,
+        filename: `INVOICE_${eventDetails[0].value}_${eventDetails[3].value}.pdf`,
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 5 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    const invoiceElement = document.getElementById('quotation-table');
+    html2pdf().set(options).from(invoiceElement).save().then(() => {
+        // invoiceElement.style.display = 'none';
+    });
+}
+
+const eventDetails = document.querySelectorAll('.eventDetails');
+window.onload = () => {
+    eventDetails.forEach((element) => {
+        element.setAttribute('autocomplete', 'off');
+    });
+
+    tallyHorizontalAmount();
+}
